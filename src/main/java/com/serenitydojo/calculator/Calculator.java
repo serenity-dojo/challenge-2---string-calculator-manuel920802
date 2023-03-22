3452756b34f7b572d44a215a5a1fa15d4d190ade
manuel76046 <kenseith92@hotmail.com>
2023-03-22 16:53:49 -0500

    Solution

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,77 @@
 package com.serenitydojo.calculator;
 
+import java.util.Stack;
+
 public class Calculator {
+
     public int evaluate(String expression) {
-        return 0;
+    int sum =0;
+    int tempSum=0;
+    int num=0;
+    char lastOp = '+';
+    for(int i = 0; i<expression.length(); i++){
+        char c = expression.charAt(i);
+     if(Character.isDigit(c)) num = num*10 + c - '0';
+     if(i == expression.length() -1 || !Character.isDigit(c) && c!=' '){
+         switch (lastOp){
+             case '+':
+                 sum+= tempSum;
+                 tempSum = num;
+                 break;
+             case '-':
+                 sum+= tempSum;
+                 tempSum = -num;
+                 break;
+             case '*':
+                 tempSum *= num;
+                 break;
+             case '/':
+                 tempSum /= num;
+                 break;
+         }
+         lastOp = c;
+         num = 0;
+        }
+    }
+    sum+=tempSum;
+    return sum;
     }
 }
+
+//throw new IllegalMathsOperatorException("Operation Not supported");
+
+// Solution using Stack
+
+//  public int evaluate(String expression) {
+//        int length;
+//        if (expression==null || (length = expression.length()) ==0) return 0;
+//        Stack<Integer> stack = new Stack<>();
+//        int num =0;
+//        char operator = '+';
+//        for(int i=0;i<length;i++){
+//            if(Character.isDigit(expression.charAt(i))){
+//                num = num*10+expression.charAt(i) -'0';
+//            }
+//            if((!Character.isDigit(expression.charAt(i)) && ' '!=expression.charAt(i)) || i==length-1){
+//                if(operator=='-'){
+//                    stack.push(-num);
+//                }
+//                if(operator=='+'){
+//                    stack.push(num);
+//                }
+//                if(operator=='*'){
+//                    stack.push(stack.pop()*num);
+//                }
+//                if(operator=='/'){
+//                    stack.push(stack.pop()/num);
+//                }
+//                operator = expression.charAt(i);
+//                num=0;
+//            }
+//        }
+//        int result = 0;
+//        for(int i:stack){
+//            result += i;
+//        }
+//        return result;
+//    }
